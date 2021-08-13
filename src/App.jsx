89240555496a5eb7537aafdcde7c121826622805
zephyrus21 <pandey.piyus@gmail.com>
2021-08-14 01:27:12 +0530

    chat feed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       projectID='82302f5a-ec40-43d1-a397-7a6a2551696e'
       userName='zephyrus21'
       userSecret='zephyrus21'
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
