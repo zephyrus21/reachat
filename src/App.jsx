52de795b051a07a09f1fb7094556438a4a429b75
zephyrus21 <pandey.piyus@gmail.com>
2021-08-14 01:04:48 +0530

    app init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height='100vh'
+      projectID='82302f5a-ec40-43d1-a397-7a6a2551696e'
+      userName='zephyrus21'
+      userSecret='zephyrus21'
+    />
+  );
+};
+
+export default App;
